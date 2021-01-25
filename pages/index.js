f3b1e76d821be98e4bf67ea182ee203f6b661a7f
yingsongsana <yingsongsana@gmail.com>
2021-01-25 13:00:41 -0500

    Install dependencies & add custom Layout comp.
    Custom Layout component in `_app.js` overrides default `App.js` used by
    Next

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Button, Alert } from "reactstrap";
+
+export default () => {  
+  return (
+    <div>
+      <div>
+        <Alert color="primary">
+          Hello Project is strapi-next with Bootstrap
+        </Alert>
+        &nbsp; <Button color="primary">Hello from nextjs</Button>
+      </div>
+    </div>
+  );
+};
